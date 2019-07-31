b4a795e716f596074105e788c7d58e6f953fc60a
Jonny Costa Alves <jcostaal@everis.com>
2019-07-31 10:57:07 -0300

    crud adicionado

--- src/main/java/br/com/empresa/springboot/controllers/InstituicoesController.java
+++ src/main/java/br/com/empresa/springboot/controllers/InstituicoesController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,4 +27,38 @@ public class InstituicoesController {
 		resultado.addObject("instituicoes", instituicoes);
 		return resultado;		
 	}
+	
+	@GetMapping("/inserir")
+	public ModelAndView inserir() {
+		ModelAndView resultado = new ModelAndView("instituicao/inserir");
+		resultado.addObject("instituicao", new Instituicao());
+		return resultado;
+	}
+	
+	@PostMapping("/inserir")
+	public String inserir(Instituicao instituicao) {
+		repositorioInstituicao.save(instituicao);
+		return "redirect:/instituicoes/index";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public ModelAndView editar(@PathVariable Long id) {
+		Instituicao instituicao = repositorioInstituicao.getOne(id);
+		ModelAndView resultado = new ModelAndView("instituicao/editar");
+		resultado.addObject("instituicao", instituicao);
+		return resultado;
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Instituicao instituicao) {
+		repositorioInstituicao.save(instituicao);
+		return "redirect:/instituicoes/index";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable Long id) {
+		repositorioInstituicao.delete(id);
+		return "redirect:/instituicoes/index";
+	}
+	
 }
