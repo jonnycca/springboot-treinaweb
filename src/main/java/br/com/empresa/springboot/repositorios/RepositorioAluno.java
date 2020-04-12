c33485a8251a811a2761147d30554b432fd43483
Jonny Christian <jonnyccalves@gmail.com>
2020-04-11 22:14:47 -0300

    Update RepositorioAluno.java

--- src/main/java/br/com/empresa/springboot/repositorios/RepositorioAluno.java
+++ src/main/java/br/com/empresa/springboot/repositorios/RepositorioAluno.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.empresa.springboot.entidades.Aluno;
 
+//@repository
 public interface RepositorioAluno extends JpaRepository<Aluno, Long> {
 
 }
